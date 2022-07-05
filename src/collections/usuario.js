e9ab65f4ef5757c46a2a5890fe923ca9c939fd8d
leandrobsb61 <leandrobsb61@gmail.com>
2022-07-05 17:46:05 -0300

    criando schema de usuário e collection para banco de dados

---
+++ src/collections/usuario.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const usuarioSchema = new mongoose.Schema(
+    { id: { type: String } },
+    { nomeUsuario: { type: String, required: true } },
+    { emailUsuario: { type: String, required: true } },
+    { senhaUsuario: { type: String, required: true } }
+);
+
+const usuariosCollection = mongoose.model('usuarios', usuarioSchema);
+
+export default usuariosCollection;
