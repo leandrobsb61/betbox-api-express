29ca2a13be17a76be7755bcc21fff845bfe31d66
leandrobsb61 <leandrobsb61@gmail.com>
2022-07-05 17:48:06 -0300

    implementando get usuários

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import db from './config/db-connect.js';
+import usuariosCollection from './collections/usuario.js';
 
 // Escutando evento de possível erro
 db.on('error', console.log.bind(console, 'Erro de conexão'));
@@ -14,8 +15,10 @@ const app = express();
 app.use(express.json());
 
 // configurando rotas com express
-app.get('/', (req, res) => {
-    res.status(200).send('Betbox API');
+app.get('/usuarios', (req, res) => {
+    usuariosCollection.find((error, usuarios) => {
+        res.status(200).json(usuarios);
+    });
 });
 
 export default app;
