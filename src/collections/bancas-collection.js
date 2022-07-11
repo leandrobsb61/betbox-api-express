98e65b21e248a2338a643f79659a7ac602493c80
leandrobsb61 <leandrobsb61@gmail.com>
2022-07-11 09:11:54 -0300

    alterando obrigatoriedade de campos

--- src/collections/bancas-collection.js
+++ src/collections/bancas-collection.js
@@ -4,8 +4,8 @@ const bancasSchema = new mongoose.Schema(
     {
         id: { type: String },
         nomeBanca: { type: String, required: true },
-        descricaoBanca: { type: String, required: true },
-        saldoBanca: { type: String, required: true },
+        descricaoBanca: { type: String },
+        saldoBanca: { type: String },
         selecionada: { type: Boolean },
         idUsuario: { type: String, required: true },
         usuarioRef: { type: mongoose.Schema.Types.ObjectId, ref: 'usuarios', required: true }
