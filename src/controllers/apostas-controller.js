16ae2d690a9b4a9be1a2295d611eafebe56cf639
leandrobsb61 <leandrobsb61@gmail.com>
2022-07-12 21:15:46 -0300

    criando serviço de listagem de apostas por banca

--- src/controllers/apostas-controller.js
+++ src/controllers/apostas-controller.js
@@ -2,6 +2,17 @@ import apostasCollection from "../collections/apostas-collection.js"
 
 export default class ApostasController {
 
+    static listarApostasPorBanca = (req, res) => {
+        const idBanca = req.query.idBanca
+
+        apostasCollection.find({ idBanca }, {}, (error, apostas) => {
+            if (error) {
+                return res.status(500).send({ message: `${error.message} - falha ao listar apostas` });
+            }
+            res.status(200).send(apostas);
+        });
+    }
+
     static cadastrarAposta = (req, res) => {
         const novaAposta = new apostasCollection(req.body);
         novaAposta.bancaRef = req.body.idBanca;
--- src/routes/apostas-routes.js
+++ src/routes/apostas-routes.js
@@ -4,6 +4,7 @@ import ApostasController from '../controllers/apostas-controller.js';
 const apostasRoutes = express.Router();
 
 apostasRoutes
+    .get('/apostas/banca', ApostasController.listarApostasPorBanca)
     .post('/apostas', ApostasController.cadastrarAposta);
 
 export default apostasRoutes;
